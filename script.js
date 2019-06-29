9adeb0c2f783f199a41d91332af41c51d3974c68
Inna Mon <inna.mon@gmail.com>
2019-06-29 12:40:56 -0500

    wrote out http request in javascript file to request instagram api info

---
+++ script.js
@@ -0,0 +1,2 @@
+https://api.instagram.com/oauth/authorize/?client_id=567a5dbd252e428aa6f40ce71717a282&redirect_uri=https://adele-decore-and-design.com&response_type=token
+567a5dbd252e428aa6f40ce71717a282
