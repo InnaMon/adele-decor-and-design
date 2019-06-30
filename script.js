1817cbb04c6224d774ea98406ff0c06106180c3b
Inna Mon <inna.mon@gmail.com>
2019-06-29 23:04:30 -0500

    added instafeed get method and script tag to fetch instagram photos

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-https://api.instagram.com/oauth/authorize/?client_id=567a5dbd252e428aa6f40ce71717a282&redirect_uri=https://adele-decore-and-design.com&response_type=token
-567a5dbd252e428aa6f40ce71717a282
+// https://api.instagram.com/oauth/authorize/?client_id=567a5dbd252e428aa6f40ce71717a282&redirect_uri=https://adele-decore-and-design.com&response_type=token
+
+// $(window).on('load', function(){
+//     $.instagramFeed({
+//     'username': 'adeledecordesign',
+//     'container': "#instagram-feed-demo",
+//     'display_profile': false,
+//     'display_biography': false,
+//     'items': 8,
+//     'items_per_row': 4,
+//     'margin': 0.5
+//     });
+// });
+
+// var instafeed = new Instafeed({
+//     clientId: '567a5dbd252e428aa6f40ce71717a282',
+//     target: 'instafeed',
+//     get: 'userId',
+//     userId: 'adeledecordesign'
+
+// });
+// instafeed.run();
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+    let tutorialFeed = new Instafeed ({
+    get: 'user',
+    userId: '12395157497',
+    clientId: '567a5dbd252e428aa6f40ce71717a282',
+    target: 'instafeed',
+    accessToken: 'Access-Token',
+    resolution: 'standard_resolution',
+    sortBy: 'most-recent',
+    limit: 8
+     });
+      
+     tutorialFeed.run();
+     
+  });
+    
