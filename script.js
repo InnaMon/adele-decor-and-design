9b52613a03c2d1035df6e5bc14bc7d890ef985ed
Inna Mon <inna.mon@gmail.com>
2019-07-05 12:26:39 -0500

    added modal code to html, css, and js files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ADD ACTIVE CLASS TO SELECTED NAV-ITEM
 const ul = document.getElementById("navbarNavAltMarkup");
 
 const navItem = ul.getElementsByClassName("nav-item");
@@ -10,11 +11,72 @@ for (let i = 0; i < navItem.length; i++) {
   });
 }
 
+//COLLAPSE HAMBURGER NAVBAR IN MOBILE/TABLET MODE WHEN SELECT NAV-ITEM
+
 $(document).ready(function () {
   $(".navbar-nav a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
 });
+
+
+// FIRST MODAL CODE
+var firstModal = document.getElementById("firstModal");
+
+var firstBtn = document.getElementById("firstBtn");
+
+var spanOne = document.getElementsByClassName("closeOne")[0];
+
+firstBtn.onclick = function(e) {
+    firstModal.style.display = "block";
+  e.preventDefault();
+}
+
+spanOne.onclick = function() {
+    firstModal.style.display = "none";
+}
+
+// SECOND MODAL CODE
+var secondModal = document.getElementById("secondModal");
+
+var secondBtn = document.getElementById("secondBtn");
+
+var spanTwo = document.getElementsByClassName("closeTwo")[0];
+
+secondBtn.onclick = function(e) {
+    secondModal.style.display = "block";
+  e.preventDefault();
+}
+
+spanTwo.onclick = function() {
+    secondModal.style.display = "none";
+}
+
+// THIRD MODAL CODE
+var thirdModal = document.getElementById("thirdModal");
+
+var thirdBtn = document.getElementById("thirdBtn");
+
+var spanThree = document.getElementsByClassName("closeThree")[0];
+
+thirdBtn.onclick = function(e) {
+    thirdModal.style.display = "block";
+  e.preventDefault();
+}
+
+spanThree.onclick = function() {
+    thirdModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == firstModal) {
+      firstModal.style.display = "none";
+    } else if (event.target == secondModal) {
+      secondModal.style.display = "none";
+    } else if (event.target == thirdModal) {
+      thirdModal.style.display = "none";
+    }
+}
 
 // added jQuery to collapse navbar in mobil view whenever nav-item clicked
 
